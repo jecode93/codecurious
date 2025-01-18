17f0081b8b49555722543d7548fe627b8a113701
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2025-01-18 00:53:35 -0500

    feat: create multiple articles with Faker data and associate with admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Creating admin user"
 admin = Admin.where(email: Rails.application.credentials.dig(:admin, :email)).first_or_initialize
 admin.update!(
   password: Rails.application.credentials.dig(:admin, :password),
@@ -14,3 +16,20 @@ admin.update!(
   name: "Jean Emmanuel Cadet",
   bio: "I specialize in designing, building, and deploying innovative software solutions for businesses as a Full-stack software engineer."
 )
+puts "Done creating admin user"
+
+puts "Creating new articles"
+ActiveRecord::Base.transaction do
+  20.times do |i|
+    puts "Creating article #{i + 1}"
+    Article.create!(
+      title: Faker::Company.name,
+      author: admin,
+      content: Faker::Lorem.sentence,
+      )
+  rescue => e
+    puts "Error on article #{i + 1}: #{e.message}"
+    raise
+  end
+end
+puts "Done creating new articles"
