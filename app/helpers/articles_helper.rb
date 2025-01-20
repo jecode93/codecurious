ddeed7cbd2769aa1d56a07f7af1f40ec91a9857c
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2025-01-20 02:16:08 -0500

    feat: add hero_text definition and show_date helper for article formatting

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,14 @@
 module ArticlesHelper
+  def hero_text = [
+    {
+      title: "Where code questions get answered",
+      description: "Welcome to my personal coding dictionary! This blog is where I document answers to my software engineering
+    questions. Whenever I forget a concept, need clarity on a term, or want to revisit a lesson, I turn here. It is
+    ideal for anyone who wants a go-to guide for solving coding problems and understanding key concepts."
+    }
+  ]
+
+  def show_date(article_date)
+    article_date.created_at.strftime("%b %d, %Y")
+  end
 end
