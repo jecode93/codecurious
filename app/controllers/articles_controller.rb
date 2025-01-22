42058ec78cacc5f18ac3f2ee87f439ffb455e79d
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2025-01-21 23:47:39 -0500

    feat: integrate Pagy for articles pagination with a limit of 12 per page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,8 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [ :show ]
+  before_action :set_article, only: [:show]
 
   def index
-    @articles = Article.descending_order
+    @pagy, @articles = pagy(Article.descending_order, limit: 12)
   end
 
   def show
