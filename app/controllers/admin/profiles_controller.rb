b5fd482d784aa5c696c1fde23de935db0bbd2627
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2025-01-27 04:47:45 -0500

    fix: add admin profile controller logic for viewing and updating profile

---
+++ app/controllers/admin/profiles_controller.rb
@@ -0,0 +1,29 @@
+class Admin::ProfilesController < ApplicationController
+  before_action :authenticate_admin!
+  before_action :set_admin, only: [:edit, :update, :index]
+
+  def index
+    @admin_articles = current_admin.articles.order(created_at: :desc)
+  end
+
+  def edit
+  end
+
+  def update
+    if @admin.update(set_params)
+      redirect_to admin_profiles_path(@admin), notice: "Admin index was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_admin
+    @admin = current_admin
+  end
+
+  def set_params
+    params.require(:admin).permit(:profile_picture, :website, :whatsapp_number, :github, :linkedin, :facebook, :instagram)
+  end
+end
