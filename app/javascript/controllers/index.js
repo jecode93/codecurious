5d0786335f75df0fc0798764c4c9806e399717b3
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2025-02-14 22:57:38 -0500

    feat: add dropdown controller to stimulus manifest

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,10 +2,14 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
+import {application} from "./application"
+import Dropdown from '@stimulus-components/dropdown'
 
 import HelloController from "./hello_controller"
+
 application.register("hello", HelloController)
 
 import RemovalsController from "./removals_controller"
+
 application.register("removals", RemovalsController)
+application.register('dropdown', Dropdown)
