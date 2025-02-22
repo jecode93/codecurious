38e33217b60b2cb6cf09305eb875c9c96c79dd53
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2025-02-22 02:06:01 -0500

    feat: integrate Pundit authorization in ApplicationController
    - Included `Pundit::Authorization` for managing user permissions.
    - Added `rescue_from Pundit::NotAuthorizedError` to handle unauthorized access.
    - Redirects users with an alert message when they attempt unauthorized actions.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,14 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
 
   include Pagy::Backend
+  include Pundit::Authorization
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(request.referrer || root_path)
+  end
 end
