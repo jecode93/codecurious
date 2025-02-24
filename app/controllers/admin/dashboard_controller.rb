e47927ecaaac0fd364e6d7b140c85d0fe7fdfaf4
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2025-02-24 03:15:16 -0500

    feat: update dashboard index with articles, article count, and author count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,13 @@
 class Admin::DashboardController < Admin::AdminController
   def index
+    @articles = Article.order(created_at: :desc).limit(6)
+    @article_count = Article.count
+    @author_count = Article.distinct.pluck(:author_id).count
+  end
+
+  private
+
+  def pundit_user
+    current_admin
   end
 end
