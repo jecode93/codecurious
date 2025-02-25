d39ce543dc5b158c4a1f5414db62163b632fae39
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2025-02-25 07:39:38 -0500

    feat: implement Admin::ArticlesController with Pundit authorization and CRUD actions

---
+++ app/controllers/admin/articles_controller.rb
@@ -0,0 +1,58 @@
+class Admin::ArticlesController < Admin::AdminController
+  before_action :set_article, only: %i[show edit update destroy]
+
+  def index
+    @articles = Article.all.order(created_at: :desc)
+  end
+
+  def show
+  end
+
+  def new
+    @article = current_admin.articles.build
+  end
+
+  def create
+    @article = current_admin.articles.build(article_params)
+    if @article.save
+      redirect_to [:admin, @article], notice: "Article was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    authorize @article, :manage?
+  end
+
+  def update
+    authorize @article, :manage?
+    if @article.update(article_params)
+      redirect_to [:admin, @article], notice: "Article was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    authorize @article, :manage?
+    @article.destroy
+    redirect_to admin_articles_url, notice: "Article was successfully destroyed."
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to admin_articles_path, alert: "Article Not found."
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :featured_image, :content)
+  end
+
+  def pundit_user
+    current_admin
+  end
+end
