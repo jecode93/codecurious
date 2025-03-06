cc59e438b2758dc62d97fa529a1f828739225e40
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2025-03-05 23:53:18 -0500

    feat: add custom 404 error route and handler

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "errors/not_found"
   get "/about", to: "pages#about"
   get "/contact", to: "pages#contact"
 
@@ -26,4 +27,9 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+
+  match '*path', to: 'errors#not_found', via: :all, constraints: lambda { |req|
+    !req.path.start_with?('/assets', '/rails/active_storage')
+  }
+
 end
