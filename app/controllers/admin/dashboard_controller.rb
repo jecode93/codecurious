b9e0f2b9f1ce61c12a088efda7acad9108da1572
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2025-03-09 01:59:19 -0500

    feat: Add article read statistics to the index page.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,6 +3,9 @@ class Admin::DashboardController < Admin::AdminController
     @articles = Article.order(created_at: :desc).limit(6)
     @article_count = Article.count
     @author_count = Article.distinct.pluck(:author_id).count
+    @total_articles_read = Article.sum(:read_count)
+    @highest_read_count = Article.maximum(:read_count)
+    @most_read_articles = Article.where(read_count: @highest_read_count).pluck(:title)
   end
 
   private
