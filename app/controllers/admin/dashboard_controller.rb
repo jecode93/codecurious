511803d91d1705650aac9b59ebaef2430de1d189
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2025-03-14 23:53:42 -0400

    fix: filter index action to show only published articles

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,11 +1,11 @@
 class Admin::DashboardController < Admin::AdminController
   def index
-    @articles = Article.order(created_at: :desc).limit(6)
-    @article_count = Article.count
-    @author_count = Article.distinct.pluck(:author_id).count
-    @total_articles_read = Article.sum(:read_count)
-    @highest_read_count = Article.maximum(:read_count)
-    @most_read_articles = Article.where(read_count: @highest_read_count).pluck(:title)
+    @articles = Article.published.order(created_at: :desc).limit(6)
+    @article_count = Article.published.count
+    @author_count = Article.published.distinct.pluck(:author_id).count
+    @total_articles_read = Article.published.sum(:read_count)
+    @highest_read_count = Article.published.maximum(:read_count)
+    @most_read_articles = Article.published.where(read_count: @highest_read_count).pluck(:title)
   end
 
   private
