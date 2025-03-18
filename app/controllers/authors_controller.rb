f134fff1e80bd9191e39914a75ecfd095dbc87bc
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2025-03-18 00:30:14 -0400

    refactor: use published scope and descending_order for author articles pagination

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,6 +1,6 @@
 class AuthorsController < ApplicationController
   def show
     @author = Admin.find(params[:id])
-    @pagy, @articles = pagy(@author.articles.order(created_at: :desc), limit: 10)
+    @pagy, @articles = pagy(@author.articles.published.descending_order, limit: 10)
   end
 end
