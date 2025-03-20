aceda85d638781b32354bc0f2716d0623ba4c551
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2025-03-20 00:04:58 -0400

    feat: add role and article status color helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,19 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  def role_color(role)
+    case role
+    when "Super admin" then "bg-blue-600"
+    when "Admin" then "bg-blue-500"
+    when "Editor" then "bg-blue-400"
+    end
+  end
+
+  def article_status_color(article)
+    case article
+    when "Published" then "bg-green-600"
+    when "Draft" then "bg-yellow-600"
+    when "Archived" then "bg-gray-600"
+    end
+  end
 end
