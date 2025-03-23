d90a744fbfac643f8af51b15116eea0517cbb770
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2025-03-23 00:53:08 -0400

    feat: add FriendlyId support for Category model, configure slugs and add association

---
+++ app/models/category.rb
@@ -0,0 +1,31 @@
+# == Schema Information
+#
+# Table name: categories
+#
+#  id          :integer          not null, primary key
+#  name        :string           not null
+#  slug        :string           not null
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+# Indexes
+#
+#  index_categories_on_name  (name) UNIQUE
+#  index_categories_on_slug  (slug) UNIQUE
+#
+
+class Category < ApplicationRecord
+  extend FriendlyId
+
+  friendly_id :name, use: %i[slugged history finders]
+
+  def should_generate_new_friendly_id?
+    slug.blank? || name_changed?
+  end
+
+  has_many :article_categories, dependent: :destroy
+  has_many :articles, through: :article_categories
+
+  validates :name, :slug, presence: true, uniqueness: true
+end
