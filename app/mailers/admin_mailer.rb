022218d2a4dc8678bf3858bff2f87e62c7b50c55
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2025-03-24 00:46:58 -0400

    feat: create welcome email for new admin registration

---
+++ app/mailers/admin_mailer.rb
@@ -0,0 +1,7 @@
+class AdminMailer < ApplicationMailer
+  def welcome_email(admin)
+    @admin = admin
+    @url = "http://admin.lvh.me:3000/login"
+    mail(to: @admin.email, subject: "Welcome to CodeCurious - #{@admin.name.titleize}")
+  end
+end
