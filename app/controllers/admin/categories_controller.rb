1122bb7037e7fc3768a62e3f68464f02dcb658c1
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2025-03-26 23:49:48 -0400

    feat: enhance admin categories controller with CRUD actions and pagination

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,22 +1,52 @@
 class Admin::CategoriesController < Admin::AdminController
+  before_action :set_category, only: [:show, :edit, :update, :destroy]
+
   def index
+    @pagy, @categories = pagy(Category.order(name: :asc))
   end
 
   def show
+    @category_articles = @category.articles
   end
 
   def new
+    @category = current_admin.categories.build
   end
 
   def create
+    @category = current_admin.categories.build(category_params)
+    if @category.save
+      redirect_to admin_categories_path, notice: 'Category was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+    if @category.update(category_params)
+      redirect_to admin_categories_path, notice: 'Category was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @category.destroy
+    redirect_to admin_categories_path, notice: 'Category was successfully destroyed.'
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to admin_categories_path, alert: "Category not found."
+  end
+
+  def category_params
+    params.require(:category).permit(:name, :description)
   end
 end
