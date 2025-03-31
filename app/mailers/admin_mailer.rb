5a7e51598f5d0b372b435c7599e3a36436ab58da
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2025-03-31 02:13:52 -0400

    feat: update welcome_email to accept generate_password and include it in the email

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,6 +1,7 @@
 class AdminMailer < ApplicationMailer
-  def welcome_email(admin)
+  def welcome_email(admin, generate_password)
     @admin = admin
+    @password = generate_password
     @url = "http://admin.lvh.me:3000/login"
     mail(to: @admin.email, subject: "Welcome to CodeCurious - #{@admin.name.titleize}")
   end
