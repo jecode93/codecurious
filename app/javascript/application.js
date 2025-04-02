1a053f4f2ac19b19f28cfde56b7f8bc9fe160ae8
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2025-04-02 00:17:59 -0400

    feat: Implement dark mode toggle with persistent state
    - Added functionality to toggle between light and dark modes
    - Updated theme toggle button to reflect previous theme settings on page load
    - Persisted theme preference using localStorage to retain user choice across sessions
    - Improved user experience with dynamic icon changes for dark and light modes

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,3 +4,40 @@ import "./controllers"
 
 import "trix"
 import "@rails/actiontext"
+
+document.addEventListener("turbo:load", function () {
+  var themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
+  var themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
+  var themeToggleBtn = document.getElementById("theme-toggle");
+
+  if (!themeToggleBtn) return; // Exit if button not found (prevents errors)
+
+  // Change the icons inside the button based on previous settings
+  if (
+    localStorage.getItem("color-theme") === "dark" ||
+    (!("color-theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    document.documentElement.classList.add("dark");
+    themeToggleLightIcon.classList.remove("hidden");
+    themeToggleDarkIcon.classList.add("hidden");
+  } else {
+    document.documentElement.classList.remove("dark");
+    themeToggleDarkIcon.classList.remove("hidden");
+    themeToggleLightIcon.classList.add("hidden");
+  }
+
+  themeToggleBtn.addEventListener("click", function () {
+    // Toggle icons
+    themeToggleDarkIcon.classList.toggle("hidden");
+    themeToggleLightIcon.classList.toggle("hidden");
+
+    // Update localStorage and class
+    if (localStorage.getItem("color-theme") === "light") {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("color-theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("color-theme", "light");
+    }
+  });
+});
