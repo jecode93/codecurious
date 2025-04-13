05fafd205ab497907fdad75af8395ec49fc82828
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2025-04-13 15:22:27 -0400

    chore: allow lvh.me subdomains in development hosts

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module Codecurious
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
+    config.hosts << "admin.lvh.me:3000"
+    config.hosts << "lvh.me:3000"
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
