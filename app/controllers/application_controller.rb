c55adb91ecde892e281aeb59df0769ab9d18bf1d
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2025-04-17 23:57:55 -0400

    feat: set custom layout for Devise sessions controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
+  layout :layout_by_resource
+
   include Pagy::Backend
   include Pundit::Authorization
 
@@ -12,5 +14,15 @@ class ApplicationController < ActionController::Base
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
     redirect_to(request.referrer || root_path)
+  end
+
+  private
+
+  def layout_by_resource
+    if devise_controller? && is_a?(Devise::SessionsController)
+      "auth"
+    else
+      "application"
+    end
   end
 end
