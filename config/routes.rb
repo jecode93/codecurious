cdfe035c201e3d6c77a23ea2df891ba8f5335eb3
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2025-04-20 23:13:26 -0400

    feat: enable dynamic PWA manifest and service worker routes

--- config/routes.rb
+++ config/routes.rb
@@ -27,8 +27,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   match "*path", to: "errors#not_found", via: :all, constraints: lambda { |req|
     !req.path.start_with?("/assets", "/rails/active_storage")
