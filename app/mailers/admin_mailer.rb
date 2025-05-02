ab0626b19ad0aa666a6e0b46004e5ed639f98084
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2025-05-01 20:15:58 -0400

    refactor(mailer): use credentials for login URL instead of hardcoded value

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -2,7 +2,7 @@ class AdminMailer < ApplicationMailer
   def welcome_email(admin, generate_password)
     @admin = admin
     @password = generate_password
-    @url = "http://admin.lvh.me:3000/login"
+    @url = Rails.application.credentials.dig(:mailing, :login_url)
     mail(to: @admin.email, subject: "Welcome to CodeCurious - #{@admin.name.titleize}")
   end
 end
